b0a16c95f717b530bb908554f16dad6df0bce014
yazekatsu <yazekatsu@sti729-1.systech-i.co.jp>
2009-07-31 13:38:20 +0900

    add sqltrace_*.sql

---
+++ oracle/sql/sqltrace_stop.sql
@@ -0,0 +1,27 @@
+/*
+$Id:$
+usage: sqlplus "/ as sysdba" @sqltrace_stop.sql
+*/
+
+set pagesize 10000
+set linesize 10000
+set trimout on
+set trimspool on
+
+set serveroutput on size 1000000
+
+declare
+	cursor cu is select sid, serial# from v$session where username is not null;
+begin
+	for rec in cu loop
+		begin
+			dbms_system.set_sql_trace_in_session(rec.sid,rec.serial#,false);
+			dbms_system.set_ev(rec.sid,rec.serial#,10046,0,'');
+		exception
+			when others then
+			dbms_output.put_line('sid:'||rec.sid||' serial#:'||rec.serial#||' sqlcode:'|| sqlcode||' sqlerrm:'||sqlerrm);
+		end;
+	end loop;
+end;
+/
+exit
