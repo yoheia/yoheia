50dd836c99ece9eedac441fdaa707aae02a177a2
yoheia <yohei.az@gmail.com>
2011-12-07 21:08:48 +0900

    add oracle/tools/join_sgastat_shared_pool.pl

---
+++ oracle/tools/join_sgastat_shared_pool.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+
+my $results={};
+my ($year, $mon, $date);
+
+print "sga component";
+
+while (defined($_ = <ARGV>)) {
+	chomp;
+	s/\t{2,}/\t/;
+	if ($. == 1) {
+		my $filename = basename($ARGV);
+		if (($year, $mon, $date) = $filename =~ /[a-z]+_(\d{4})(\d{2})(\d{2})_\d+\.log/) {
+			print "$year-$mon-$date\n";
+		} else {
+			close ARGV;
+		}
+	}
+	my @cols = split(/\t/, $_);
+	$results->{$cols[0]}->{"$year-$mon-$date"} = $cols[1] if /^shared pool/;
+	close ARGV if eof;
+}
+
+print "\n";
+
+foreach (keys %{$results}) {
+	print $_;
+	foreach my $date (keys %{$results->{$_}}) {
+		my $val = ($results->{$_}->{$date} ne '' ? $results->{$_}->{$date} : 0 );
+		$val =~ s/\s//g;
+		print "\t$val";
+	}
+	print "\n";
+}
