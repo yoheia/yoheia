131a570cd59f7153238d59ab8c918e151724b586
yoheia <yohei.az@gmail.com>
2016-12-07 00:40:51 +0900

    spool stats sripts

---
+++ oracle/sql/user_tab_col_statistics.sql
@@ -0,0 +1,20 @@
+set arraysize 500
+set pagesize 50000
+set linesize 32767
+set trimout on
+set trimspool on
+set termout off
+set feedback off
+set colsep '|'
+
+alter session set NLS_DATE_FORMAT='YYYY-MM-DD HH24MISS';
+alter session set NLS_TIMESTAMP_FORMAT='YYYY-MM-DD HH24MISS';
+
+col spool_file_name new_value spool_file_name for a100
+select 'user_tab_col_statistics_'||to_char(sysdate,'YYYYMMDD-HH24MISS') ||'.csv' spool_file_name from dual;
+spool &spool_file_name
+
+select * from user_tab_col_statistics;
+
+spool off
+
