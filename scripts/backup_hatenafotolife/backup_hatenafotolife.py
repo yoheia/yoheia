b20ca250d57608eddba7441dad0b9a049b7f2fab
yoheia <yohei.az@gmail.com>
2016-12-31 10:31:53 +0900

    defined lambda_handler

--- scripts/backup_hatenafotolife/backup_hatenafotolife.py
+++ scripts/backup_hatenafotolife/backup_hatenafotolife.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
 import os
 import boto3
 import urllib2
 from bs4 import BeautifulSoup
 
 def lambda_handler(event, context):
-	s3_bucket_name = 'yoheia'
-	work_dir = '/tmp'
+    target_uri='http://f.hatena.ne.jp/yohei-a/'
+    s3_bucket_name = 'hatenafotolife'
+    work_dir = '/tmp'
+    
+    html = urllib2.urlopen(target_uri)
+    soup = BeautifulSoup(html)
+    images = soup.find_all(class_='foto_thumb')
+    for img in images:
+        img_uri = img['src']
+        img_filename = os.path.basename(img_uri)
+        s3 = boto3.client('s3')
+        r = urllib2.urlopen(img_uri)
+        f = open(work_dir + '/' + img_filename, "wb")
+        f.write(r.read())
+        f.close()
+        s3.upload_file(work_dir + '/' + img_filename, s3_bucket_name, img_filename)
 
-	html = urllib2.urlopen('http://f.hatena.ne.jp/yohei-a/')
-	soup = BeautifulSoup(html)
-	images = soup.find_all(class_='foto_thumb')
-
-	for img in images:
-		url = img['src']
-		img_filename = os.path.basename(img['src'])
-		s3 = boto3.client('s3')
-		print img['src'], os.path.basename(img['src'])
-		r = urllib2.urlopen(img['src'])
-		f = open(work_dir + '/' + os.path.basename(img['src']), "wb")
-		f.write(r.read())
-		f.close()
-		s3.upload_file(work_dir + '/' + img_filename, s3_bucket_name, img_filename)
-
-	retrun "OK"
+    return 1
