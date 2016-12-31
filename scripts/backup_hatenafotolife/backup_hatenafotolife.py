ff2860afad78ba537b1ab40cf45d0edcaab2a4f7
yoheia <yohei.az@gmail.com>
2016-12-31 18:29:42 +0900

    changed to parse RSS

--- scripts/backup_hatenafotolife/backup_hatenafotolife.py
+++ scripts/backup_hatenafotolife/backup_hatenafotolife.py
@@ -1,24 +1,26 @@
+# -*- coding: utf-8 -*-
 import os
+import re
 import boto3
 import urllib2
 from bs4 import BeautifulSoup
 
 def lambda_handler(event, context):
-    target_uri='http://f.hatena.ne.jp/yohei-a/'
+    target_uri= 'http://f.hatena.ne.jp/yohei-a/rss'
     s3_bucket_name = 'hatenafotolife'
     work_dir = '/tmp'
-    
     html = urllib2.urlopen(target_uri)
-    soup = BeautifulSoup(html)
-    images = soup.find_all(class_='foto_thumb')
-    for img in images:
-        img_uri = img['src']
-        img_filename = os.path.basename(img_uri)
-        s3 = boto3.client('s3')
-        r = urllib2.urlopen(img_uri)
-        f = open(work_dir + '/' + img_filename, "wb")
-        f.write(r.read())
-        f.close()
-        s3.upload_file(work_dir + '/' + img_filename, s3_bucket_name, img_filename)
+    soup = BeautifulSoup(html, 'html.parser')
 
+    s3 = boto3.client('s3')
+    for item in soup.find_all("hatena:imageurl"):
+    	img_uri = item.contents[0]
+    	img_filename = os.path.basename(img_uri)
+    	r = urllib2.urlopen(img_uri)
+    	f = open(work_dir + '/' + img_filename, "wb")
+    	f.write(r.read())
+    	r.close()
+    	f.close()
+        s3.upload_file(work_dir + '/' + img_filename, s3_bucket_name, img_filename)
+    
     return 1
