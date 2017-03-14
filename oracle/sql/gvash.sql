de8b8ae5b0e1a8b9ad10bb442b781e111a5d287b
yoheia <yohei.az@gmail.com>
2017-03-15 07:09:09 +0900

    gv$active_session_history script

---
+++ oracle/sql/gvash.sql
@@ -0,0 +1,19 @@
+set pagesize 50000
+set linesize 32767
+set trimout on
+set trimspool on
+set termout off
+set feedback off
+set colsep '|'
+
+alter session set NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS';
+alter session set NLS_TIMESTAMP_FORMAT='YYYY-MM-DD HH24:MI:SS';
+
+col spool_file_name new_value spool_file_name for a100
+select 'gvash_'|| to_char(sysdate,'YYYYMMDD-HH24MISS') || '.csv' spool_file_name from dual;
+spool &spool_file_name
+
+select * from gv$active_session_history order by sample_id;
+
+spool off
+
