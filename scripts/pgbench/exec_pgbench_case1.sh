0b391a6155f222b333ad4aaf6ca9743b767dc14c
yoheia <yohei.az@gmail.com>
2017-11-05 12:20:43 +0900

    modified exec_pgbench_case1.sh

--- scripts/pgbench/exec_pgbench_case1.sh
+++ scripts/pgbench/exec_pgbench_case1.sh
@@ -3,5 +3,5 @@
 export LANG=C
 BASE_DIR=$(cd $(dirname $0);pwd)
 
-bash ${BASE_DIR}/pgbench_wrapper.sh mydbinstance.cnaamhj3erpx.ap-northeast-1.rds.amazonaws.com mydb awsuser Password
+bash ${BASE_DIR}/pgbench_wrapper.sh hostname dbname username password
 
