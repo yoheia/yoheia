78b0d4bcfa69bf6a8c0096720d6e435a56ca2a45
yoheia <yohei.az@gmail.com>
2020-07-02 05:13:10 +0000

    apg_activitystream_json2csv_from-s3.sh

--- aws/apg-activitystream/apg-activity-stream-s3-csv2ftp/apg_activitystream_json2csv_from-s3.sh
+++ aws/apg-activitystream/apg-activity-stream-s3-csv2ftp/apg_activitystream_json2csv_from-s3.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 export LANG=C
 
-export FTP_SERVER='172.*.*.*'
-export FTP_USER='ftpuser'
-export FTP_PASSWD='hoge'
-export FTP_REMOTE_DIR='/home/ftpuser/'
-export CSVFILE_PREFIX='aurora-postgres-activity.csv'
-export S3_BUCKET='aurora-postgres-activity-stream-az-tokyo'
-export S3_PREFIX='aws-rds-das-cluster-KGKBVXA3OLPRPZH66XFBDPWQUI'
+export CSVFILE_PREFIX='aurora-postgres-activity_'
+export S3_BUCKET='s3_bucke_name'
+export S3_PREFIX='aws-rds-das-cluster-***'
 export LOCAL_DIR='./log'
 export CSV_DIR='./csv'
-export KMS_KEY_ID='0b5af1d9-4ce2-40c0-8a48-1a28b6bcd2b0'
-export KINESIS_STREAM_NAME='aws-rds-das-cluster-KGKBVXA3OLPRPZH66XFBDPWQUI'
+export KMS_KEY_ID='***-***-***-***--***'
+export KINESIS_STREAM_NAME='aws-rds-das-cluster-***'
 export REGEION='ap-northeast-1'
-export APG_CLUSTER_ID='cluster-KGKBVXA3OLPRPZH66XFBDPWQUI'
+export APG_CLUSTER_ID='cluster-***'
 
-python apg_activitystream_s3_csv2ftp.py
+python apg_activitystream_json2csv_from-s3.py
