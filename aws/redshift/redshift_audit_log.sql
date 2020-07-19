4cbd40ce86a51ed1d5ec12b88cb9c6a6ac974cd9
yoheia <yohei.az@gmail.com>
2020-07-19 16:47:33 +0900

    fixed duplication problem

--- aws/redshift/redshift_audit_log.sql
+++ aws/redshift/redshift_audit_log.sql
@@ -1,43 +1,53 @@
-/* returned_rows */ select c.starttime
+/* returned_rows */ select max(d.starttime) as starttime
+	,max(d.endtime) as endtime
+	,'Redshift' as servie_type
+	,max(d.database)  as database 
+	,max(d.userid)  as userid
+	,d.query
+	,max(d.sql) as sql
+	,max(d.rows) as rows
+	,max(d.pid) as pid
+    ,max(g.remotehost) as remotehost
+    ,max(g.username) as username
+from (select c.starttime
 		,c.endtime
-		,d.remotehost
-        ,'Redshift' servie_type
+		,'Redshift' servie_type
 		,c.database
 		,a.userid
-        ,d.username
-        ,a.query
-        ,a.substring sql
-        ,b.rows
+		,a.query
+		,a.substring sql
+		,b.rows
+		,c.pid
 from svl_qlog a join stl_return b
-                on a.query=b.query
-        join stl_query c
-        on c.query = a.query
-        join stl_connection_log d
-        on d.pid = c.pid
+				on a.query=b.query
+		join stl_query c
+		on c.query = a.query
 where
-    b.slice >= 6411
-    and a.userid != 1
-    and c.starttime between '2020-04-21 11:30' and '2020-04-21 12:00'
-    and sql not like '/* returned_rows */%'
+	b.slice >= 6411
+	and a.userid != 1
+	and sql not like '/* returned_rows */%'
 union all
 select  c.starttime
 		,c.endtime
-		,d.remotehost
-        ,'Redshift' servie_type
+		,'Redshift' servie_type
 		,c.database
 		,a.userid
-        ,d.username
-        ,a.query
-        ,a.substring sql
-        ,b.rows
+		,a.query
+		,a.substring sql
+		,b.rows
+		,c.pid
 from svl_qlog a join stl_return b
-        on a.source_query=b.query
-        join stl_query c
-        on c.query = a.query
-        join stl_connection_log d
-        on d.pid = c.pid
+		on a.source_query=b.query
+		join stl_query c
+		on c.query = a.query
 where
-    sql not like '/* returned_rows */%'
-    and c.starttime between '2020-04-21 11:30' and '2020-04-21 12:00'
-order by
-query desc
+	sql not like '/* returned_rows */%') d left outer join 
+		(select e.pid, e.remotehost, e.username, f.usesysid, e.recordtime
+			from stl_connection_log e 
+				join SVL_USER_INFO f 
+					on e.username = f.usename
+			where e.event = 'authenticated' ) g
+on d.pid = g.pid 
+	and d.userid = g.usesysid 
+	and d.starttime > g.recordtime
+group by query;
