dd92d28b41a02d8148afdf50f591ecbf4c7f6386
Yohei <yohei.az@gmail.com>
2022-04-21 08:33:10 +0900

    Update get_env_info.sql

--- aws/redshift/redshift_measuring_query_exec_time/get_env_info.sql
+++ aws/redshift/redshift_measuring_query_exec_time/get_env_info.sql
@@ -13,4 +13,11 @@ select version();
 -- show table stats
 select * from svv_table_info;
 
+-- show sort keys
+set search_path to '$user', public;
+select * from pg_table_def
+where sortkey <> 0
+and schemaname  not in ('pg_catalog')
+order by schemaname, tablename, sortkey;
+
 \q
