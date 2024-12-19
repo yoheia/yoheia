5b664177e33c44d57ffb788db373f65b4d0b2fb6
yoheia <yohei.az@gmail.com>
2024-12-19 04:08:11 +0000

    updated pg_table_def.sql

--- aws/redshift/get_redshift_sys_table_view/sql/pg_table_def.sql
+++ aws/redshift/get_redshift_sys_table_view/sql/pg_table_def.sql
@@ -1,4 +1,4 @@
-set search_path to '$user', public, shp_wk, shp, sys_calendar, vshp, xse, trv, works, stable, ywh, vdb, dev_shp, members_mstr, settlement, settle_db, dwhlib;
+set search_path to '$user', public;
 select * from pg_table_def
 where schemaname not in ('pg_catalog')
 order by schemaname, tablename, sortkey;
